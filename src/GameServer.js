24bcfac60432b18f66915a8d128eb40cbb1657ba
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-09-27 16:09:02 -0500

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -504,7 +504,7 @@ GameServer.prototype.splitCells = function(client) {
         // Calculate mass of splitting cell
         var newMass = cell.mass / 2;
         cell.mass = newMass;
-	    removeNode(cell);
+this.removeNode(e);
         // Create cell
         split = new Entity.PlayerCell(this.getNextNodeId(), client, startPos, newMass);
         split.setAngle(angle);
